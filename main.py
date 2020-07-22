f29b5e5ee511cf56e096b63505a5e5155f93ba41
Asiaron <aslipenchuk@yandex.ru>
2020-07-22 19:24:06 +0300

    adapt to python 2

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
-from collections import Iterable
+# from collections import Iterable
 from itertools import product
 
 
-def generate_expression() -> Iterable[str]:
+def generate_expression(): # -> Iterable[str]:
     for signs in product({'+', '-', ''}, repeat=10):
         if signs[0] == '+':
             continue
         yield '{}9{}8{}7{}6{}5{}4{}3{}2{}1{}0'.format(*signs)
 
 
-def evaluate(expression: str) -> int:
-    if (plus_pos := expression.find('+')) >= 0:
+def evaluate(expression):  #  : str): -> int:
+    plus_pos = expression.find('+')
+    minus_pos = expression.rfind('-')
+    if plus_pos >= 0:
         return evaluate(expression[:plus_pos]) + evaluate(expression[plus_pos+1:])
-    elif (minus_pos := expression.rfind('-')) >= 0:
+    elif minus_pos >= 0:
         # minus is right associative
         return evaluate(expression[:minus_pos]) - evaluate(expression[minus_pos+1:])
     else:
@@ -24,7 +26,7 @@ def evaluate(expression: str) -> int:
 def main():
     for expression in generate_expression():
         if evaluate(expression) == 200:
-            print(f'{expression} = 200')
+            print(expression + ' = 200')
 
 
 if __name__ == '__main__':
